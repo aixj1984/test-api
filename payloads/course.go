30c2410fae52b40e399658efcd98fcf995151181
unknown <richard.ai@blackshark.com>
2018-02-05 18:51:41 +0800

    fix uid

--- payloads/course.go
+++ payloads/course.go
@@ -2,7 +2,7 @@ package payloads
 
 type SaveCustomerCoursesSetting struct {
 	DefalutCourses string `validate:"required"`
-	CustomerId     int    `validate:"required"`
+	//CustomerId     int    `validate:"required"`
 }
 
 type PurchaseCourses struct {
